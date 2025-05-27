b35120b65c6954986cb1beadb31556dda723ef4b
Tim Roberts <tim@timjroberts.com>
2025-05-27 20:14:16 +0100

    Fixup styling issues

--- packages/web-app/src/components/cards/post-summary/index.tsx
+++ packages/web-app/src/components/cards/post-summary/index.tsx
@@ -16,20 +16,19 @@ export const PostSummaryCard: React.FC<PostSummaryCardProps> = ({post}: PostSumm
       {new Date(post.timeStamp).toLocaleDateString("en-US", {month: "short", day: "numeric", year: "numeric"})}
     </div>
     <Link to={`/posts/${post.slug}`} className="text-black">
-    <div className={`summary ${post.hasThumbNail ? "col-span" : "col-span-2"}`}>
-      
-        <div className="title">{post.title}</div>
-        {post.summary && <div className="text">{post.summary}</div>}
-      
-      <div className="footer">
-        <div><Icon name="clock" size="large" /> {post.readTime}</div>
+      <div className={`summary ${post.hasThumbNail ? "col-span" : "col-span-2"}`}>
+          <div className="title">{post.title}</div>
+          {post.summary && <div className="text">{post.summary}</div>}
+        
+        <div className="footer">
+          <div><Icon name="clock" size="small" /> {post.readTime}</div>
+        </div>
+        <div className="categories">
+          {post.category.map(
+            category => <CategoryLink key={category} slug={category} />
+          )}
+        </div>
       </div>
-      <div className="categories">
-        {post.category.map(
-          category => <CategoryLink key={category} slug={category} />
-        )}
-      </div>
-    </div>
     </Link>
     {post.hasThumbNail && 
       <div className="thumbnail">
